9ca994c0013cf72c4fcd64c7a92ba17437b668f2
unknown <christian@stikman.com>
2021-04-14 22:44:22 -0700

    Write some js variables and better html

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+// My variables
+var h1El = document.querySelector('#h1');
+var divEl = document.querySelectorAll('.questions');
+var timerEl = document.querySelector('.timer');
+
+// Functions needed
+function welcome() {};
+
+function timer() {};
+
+function startQuiz() {};
+
+function submitScore() {};
+
+function viewScores() {};
+
