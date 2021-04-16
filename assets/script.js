206edd92e5ec476023ca086b22d4f4f1f8f6ee3f
unknown <christian@stikman.com>
2021-04-16 00:13:24 -0700

    Add js for timer to begin on start button

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,31 @@
 var h1El = document.querySelector('#h1');
 var divEl = document.querySelectorAll('.questions');
 var timerEl = document.querySelector('.timer');
+var startBtn = document.querySelector('.startButton');
+
+var secondsLeft = 10;
 
 // Functions needed
 function welcome() {};
 
-function timer() {};
+function timerStart() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timerEl.textContent = secondsLeft + " seconds left!";
 
-function startQuiz() {};
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            alert("You have run out of time!");
+            //log score and input name function
+        }
+
+    }, 1000);
+};
+
+//function startQuiz() {};
+startBtn.addEventListener('click', timerStart);
+
+function endGame() {};
 
 function submitScore() {};
 
